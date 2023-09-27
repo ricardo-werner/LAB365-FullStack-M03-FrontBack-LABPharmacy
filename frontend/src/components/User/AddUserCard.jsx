392975a2585b6f36991a9c86605f8bcd72cc2a79
ricardo-werner <wernerdive@gmail.com>
2023-09-27 19:10:59 -0300

    feat: add dados no onSubmit

--- frontend/src/components/User/AddUserCard.jsx
+++ frontend/src/components/User/AddUserCard.jsx
@@ -3,10 +3,14 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import axios from "axios";
 import "../../components/User/AddUserCard.css";
 
 const schema = yup
   .object({
+    name: yup
+      .string()
+      .required("O nome é obrigatório"),
     email: yup
       .string()
       .email("Digite um email válido")
@@ -26,54 +30,49 @@ function AddUser() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
   });
 
-  console.log(watch("email"));
-
   function onSubmit(userData) {
-    fetch('http://localhost:5000/users', {
-      method: 'POST',
-      body: JSON.stringify(userData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(userData);
-    alert("Cadastro realizado com sucesso !!!!")
-    
+    axios.post('http://localhost:3333/api/createOneUsuario', userData)
+      .then(response => {
+        console.log(response.data);
+        alert("Cadastro realizado com sucesso !!!!")
+      })
+      .catch(error => {
+        console.log(error);
+        alert("Não foi possível cadastrar o usuário, tente novamente.");
+      });
+
   };
-  
+
   const history = useNavigate();
   function handleGoBack() {
     history(-1);
-}
+  }
 
   return (
-    <form  className="add-form-container"  onSubmit={handleSubmit(onSubmit)}>
+    <form className="add-form-container" onSubmit={handleSubmit(onSubmit)}>
       <h2 className="add-titulo">Cadastro de Novo Usuário</h2>
-      {/*<label>
-        Nome
-        <input type="text" {...register("name", { required: true })} />
-        <span>{errors.name?.message}</span>
-      </label>*/}
       <div>
         <fieldset className="add-field-master">
+          <label>
+            Nome
+            <input type="text" {...register("name", { required: true })} />
+            <span>{errors.name?.message}</span>
+          </label>
           <label>
             Email
             <input type="text" {...register("email")} />
             <span>{errors.email?.message}</span>
           </label>
-
           <label>
             Senha
             <input type="password" {...register("password")} />
             <span>{errors.password?.message}</span>
           </label>
-
           <label>
             Confirmar Senha
             <input type="password" {...register("confirmPassword")} />
